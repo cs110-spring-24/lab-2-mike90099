ac5c269521183ba579f8936f4a4a361751bbc848
mike90099 <malvarez12@dons.usfca.edu>
2024-03-01 23:45:31 -0800

    Hello, I'm just pushing what I have. I spent all my time trying to figure out lab 3 with little to nothing spent on this one.

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -4,9 +4,21 @@ num = random.randint(1, 100)
 user = input("Enter your guess: ")
 user = int(user)
 
-if user > num:
+#loop for whole guess
+ 
+while user > num:
     print("Too high, it was", num)
-elif user < num:
-    print("Too low, it was", num)
-else:
-    print("You got it!")
+    print("Too high!")
+    print("Guess a number between...",)
+    print((num),"and",())
+if user > num:
+    print("Too high!")
+    print("Guess a number between...",)
+    print((num),"and",(num+50))
+count = 0
+while count < 500:
+    count =count + 1
+    if count % 4 == 0:
+        continue
+    else:
+        print(count)
